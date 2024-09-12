10609debb5666605f93a0ad829a197996f7b093e
Michael <michaeledwards2009@gmail.com>
2024-09-12 13:41:17 -0400

    remove console.log

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,7 +5,6 @@ export const dynamic = "force-dynamic";
 
 export default async function Home() {
   const images = await getImages();
-  console.log(images);
 
   return (
     <div className="font-[family-name:var(--font-geist-sans)] p-4">
